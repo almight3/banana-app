b1e85d3c325d48d57f26abb968c6173b8c13cb83
almight3 <shrivastavap136@gmail.com>
2021-07-31 01:00:41 +0530

    features

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ let textOutput = document.querySelector("#translate-output");
 var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 
-// joinin text (givin query to link)  to gettranslated function 
+// comstructing text (givin query to link)  to gettranslated function 
 function getTranslatedUrl(text){
     return serverUrl + "?" + "text=" + text; 
 
 }
-// this section is for catching and alert clint if get error 
+// this section is for catching and alert clinte if get error 
 function errorHandler(error){
     console.log("there is error :" + error);
     alert("request time out try again after siome time");    
@@ -23,8 +23,8 @@ function errorHandler(error){
 
 function clickHandler(){
     var inputText = textInput.value;//Taking Input
-    fetch(getTranslatedUrl(inputText)) // fetching Complete text  by calling get transalatedURl function
-        .then(response => response.json()) //cahnging respone to response.json
+    fetch(getTranslatedUrl(inputText)) //using  fetch for caliing api 
+        .then(response => response.json()) //changing response to response.json
         .then(json =>{                                    
             let translatedText = json.contents.translated; //selecte translated section and and store it in translateText variable      
             textOutput.innerText = translatedText; // using innertext method to show translated text on div section//
@@ -33,5 +33,4 @@ function clickHandler(){
 
 }
 
-btnTranslate.addEventListener("click", clickHandler);//function call whenever click event happen
 btnTranslate.addEventListener("click", clickHandler);//function call whenever click event happen
